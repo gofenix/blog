94d2be17fd3243b6c5b1d5da09b266daa5f38dc8
zzf <1241943309@qq.com>
2017-08-27 13:45:50 +0800

    task and tag

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import local
+from fabric.decorators import task
 
 def hello():
     print("hello world!")
@@ -7,5 +8,18 @@ def hello():
 def install():
     local("pip install -r requirements.txt")
 
+@task
 def runserver():
+    "Run server"
     local("./growth_studio/manage.py runserver 0.0.0.0:8000")
+
+@task
+def tag_version(version):
+    "Tag new version"
+    local("git tag %s" %version)
+    local("git push origin %s" %version)
+
+@task
+def fetch_version(version):
+    "Fetch git version"
+    local("wget https://codeload.github.com/zhenfeng-zhu/blog/tar.gz/%s" %version)
